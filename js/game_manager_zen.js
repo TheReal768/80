39ec1627fdcb58c08c06456817895d2a98aa9f1a
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-12-12 19:23:57 -0600

    80 - Increment by 8

--- js/game_manager_zen.js
+++ js/game_manager_zen.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 8 : 16;
+    var value = Math.random() < 0.92 ? 8 : 16;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
